22c1480c963cb1d6ad8da995dff7b1cc0567fbc1
Thomas <thsp@eaaa.dk>
2024-10-22 12:43:59 +0200

    resolved merge conflicts

--- modul-04/GitHubStuff/script.js
+++ modul-04/GitHubStuff/script.js
@@ -2,8 +2,4 @@ console.log("success");
 
 let start = document.getElementById("beginning");
 
-<<<<<<< HEAD
 start.innerHTML = "<h1>Welcome</h1> <p>Hello to you - my name is Jane Doe</p>";
-=======
-start.innerHTML = "<h1>Welcome</h1> <p>Hello to you - my name is Jane Doe</p>";
->>>>>>> 6f358544176b2253f40de666930de9f2aadb8948
