5e6cc6fb74879f7e0f5c78951cb47f2b0198b30a
Thomas <thsp@eaaa.dk>
2024-10-22 11:51:57 +0200

    Added my full name

--- modul-04/GitHubStuff/script.js
+++ modul-04/GitHubStuff/script.js
@@ -2,4 +2,4 @@ console.log("success");
 
 let start = document.getElementById("beginning");
 
-start.innerHTML = "<h1>Welcome</h1> <p>Hello to you - my name is Jane</p>";
+start.innerHTML = "<h1>Welcome</h1> <p>Hello to you - my name is Jane Doe</p>";
