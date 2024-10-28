1e0dba322230b597fc608fc15e484a8974c9ce08
Thomas <thsp@eaaa.dk>
2024-10-28 10:00:57 +0100

    After code-labelling

--- modul-05/Code-Labelling/code-labelling.js
+++ modul-05/Code-Labelling/code-labelling.js
@@ -1,11 +1,21 @@
-for (let i = 2; i < 21; i += 2) {}
+/* (let i = 2; i < 21; i += 2) {}
 
 function sayHello(name, language) {
   console.log(name + language);
 }
 
-sayHello("John Smith", "Galifreyan");
+sayHello("John Smith", "Galifreyan");*/
 
-const car = { make: "Ford", model: "Mustang", year: 1969 };
+const car = { make: "Ford", model: "Mustang", year: 1969, mileage: 9999 };
+
+console.log(car.make);
+
+car.mileage = 99999;
+
+console.log(car);
+
+car.mak = "John Deer";
+
+console.log(car);
 
 //End of file
